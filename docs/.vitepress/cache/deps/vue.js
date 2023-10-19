3da5ce44e2fcc2419d56faae5973b36aecae140d
lhdai <d_muses@163.com>
2023-10-19 10:31:52 +0800

    update

--- docs/.vitepress/cache/deps/vue.js
+++ docs/.vitepress/cache/deps/vue.js
@@ -59,7 +59,6 @@ import {
   hydrate,
   initCustomFormatter,
   initDirectivesForSSR,
-  init_vue_runtime_esm_bundler,
   inject,
   isMemoSame,
   isProxy,
@@ -154,9 +153,7 @@ import {
   withMemo,
   withModifiers,
   withScopeId
-} from "./chunk-D4ROFVM7.js";
-import "./chunk-ROME4SDB.js";
-init_vue_runtime_esm_bundler();
+} from "./chunk-67UUJLDS.js";
 export {
   BaseTransition,
   BaseTransitionPropsValidators,
